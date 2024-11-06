062e5ab7e73ecdbc07477e37b9710a48d406d12c
bin <2484062938@qq.com>
2024-11-07 00:32:01 +0800

    Site updated: 2024-11-07 00:31:58

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/01/第一份博客/"];function toRandomPost(){
+var posts=["2024/11/06/html基本骨架/","2024/11/01/第一份博客/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
