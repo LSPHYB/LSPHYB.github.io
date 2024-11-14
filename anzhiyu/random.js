5707ea8650eceb92b029dfae3d381fbbcc1b1658
bin <2484062938@qq.com>
2024-11-15 00:15:12 +0800

    Site updated: 2024-11-15 00:15:07

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/06/html基本骨架/","2024/11/01/第一份博客/"];function toRandomPost(){
+var posts=["2024/11/06/html基本骨架/","2024/11/14/关于VMWARE无法拖拽文件以及复制粘贴问题的解决方法/","2024/11/01/第一份博客/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
